347ff3944d045be39920cfe13ee8a15c3e6e96fe
fromjkeee16 <fromjkeee16@gmail.com>
2025-03-30 22:01:36 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-// write your code here
+const HEADING_TO_FIND = 'position';
+
+function findColIndex(tableElement, str) {
+  const theadCells = tableElement.querySelector('thead tr').children;
+
+  for (let i = 0; i < theadCells.length; i++) {
+    if (theadCells[i].textContent.toLowerCase() === str) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+function copyColumn(element, index) {
+  if (index === -1) {
+    return;
+  }
+
+  const rows = element.querySelectorAll('tr');
+  const colNumber = rows[0].cells.length;
+
+  if (!(rows && colNumber)) {
+    return;
+  }
+
+  for (const row of rows) {
+    const lastElement = row.cells[colNumber - 1];
+    const newElement = lastElement.cloneNode(true);
+
+    newElement.textContent = row.cells[index].textContent;
+
+    row.insertBefore(newElement, lastElement);
+  }
+}
+
+const table = document.querySelector('table');
+const headingIndex = findColIndex(table, HEADING_TO_FIND);
+
+copyColumn(table, headingIndex);
